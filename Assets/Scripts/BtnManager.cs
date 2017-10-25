cfb371d82dac6d1896627b84a0b95c1cfa706e58
Branaman <Mbranaman89@gmail.com>
2017-10-25 12:10:55 -0700

    first commmit

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnManager : MonoBehaviour {
+
+	public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+}
