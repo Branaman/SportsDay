5be755e5424de4abadd7d0d01bba97c2d05160a5
Branaman <Mbranaman89@gmail.com>
2017-10-26 12:19:02 -0700

    fnished navigation

---
+++ Assets/Scripts/NavMap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavMap : MonoBehaviour
+{
+
+    GameObject player;
+    public string levelToLoad;
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        if (other.gameObject == player)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
